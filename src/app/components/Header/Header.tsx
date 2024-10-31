64d9f1d7951eecc5c6c4803c00cd613a76f169b1
Davi Araújo <davi.lucas350@gmail.com>
2024-10-31 14:36:23 -0300

    refactor: corrigindo o componente Header para ocupar toda a página e definir o espaçamento com padding

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,41 +1,39 @@
 import React from 'react';
 import NavBar from './NavBar';
-import ImgBackground from '@/assets/pet.jpg'; // Importa a imagem de fundo
-import Image from 'next/image'; // Importa o componente Image do Next.js para otimização de imagens
+import Image from 'next/image';
 
 interface HeaderProps {
-  title: string; // Define o título que será exibido no header, passado como prop
+  title: string;
 }
 
-// Componente Header que recebe um título como prop
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <>
       <header
         data-layername="header"
-        className="relative border border-primary flex overflow-hidden flex-col items-center px-20 pt-20 w-full bg-zinc-800 bg-opacity-20 max-md:px-5 max-md:max-w-full"
+        className="relative flex overflow-hidden flex-col items-center justify-center w-full h-screen bg-zinc-800 bg-opacity-20"
       >
         {/* Camada semi-transparente para escurecer a imagem de fundo */}
         <div className="absolute inset-0 bg-black bg-opacity-50 z-0" />
-
-        {/* Componente Next.js Image para otimizar o carregamento e a responsividade da imagem de fundo */}
+       
         <Image
-          fill // Preenche todo o container do componente
-          src="/pet.jpg" // Caminho da imagem
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Define tamanhos responsivos para otimizar o carregamento
+          alt=""
+          src="/pet.jpg"
+          fill
+          className="object-cover" // Ajusta a imagem para cobrir todo o container
+          sizes="100vw"
         />
 
-        <div className="relative flex z-10 flex-col -mb-8 w-full max-w-[1212px] max-md:mb-2.5 max-md:max-w-full">
-          {/* Componente NavBar - navegação da página */}
+        <div className="relative z-10 flex flex-col w-full max-w-6xl p-5">
           <NavBar />
     
-          {/* Container do título e ajustes de responsividade */}
-          <div className="flex flex-wrap gap-5 justify-between w-full h-full mt-[400px] max-md:mt-10 max-md:max-w-full">
+          {/* Container do título */}
+          <div className="flex flex-wrap gap-5 justify-between w-full mt-64">
             <h1
               data-layername="garantindoQueSeusAnimaisDeEstimacaoTenhamUmaVidaMelhor"
               className="text-4xl font-bold text-black drop-shadow-lg relative max-md:text-2xl"
             >
-              {title} {/* Exibe o título recebido via prop */}
+              {title}
             </h1>
           </div>
         </div>
