b5ef9dd199bd3c5562637d33ad9ca696551021d6
hudney-fsbrito <hudney.rse@gmail.com>
2024-11-01 14:58:03 -0300

    Refactor: Add + variáveis de córes no arquivo tailwuind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,10 @@ module.exports = {
       colors: {
         primary: "#4CAF50", // Principal
         secondary: "#FFC107", // Secundária
+        tertiary: "#2195E8", //Terciária
         accent: "#FF5722", // Cor de destaque
+        accentTwo: "#7E8A8C", // Cor de destaque
+        accentThree: "#EBEDED", // Cor de destaque
       },
       fontFamily: {
         sans: ["Roboto", "Helvetica", "Arial", "sans-serif"], // Fonte
