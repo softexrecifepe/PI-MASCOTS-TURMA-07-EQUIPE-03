a5544e2e254ceb031ba4dc0cfa54a80988b4086b
hudney-fsbrito <hudney.rse@gmail.com>
2024-11-01 13:56:01 -0300

    feat: Add botão "+ Consultas"

--- src/app/management/page.tsx
+++ src/app/management/page.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import Logo from "../../../public/logo.svg";
 import PawVet from "../../../public/veterinary_paw.svg";
-import Notification from "../../../public/bell.svg"
+import Notification from "../../../public/bell.svg";
 import Image from "next/image";
 import Input from "../components/Input";
+import { Button } from "../components/Button";
 
 export const Header: React.FC = () => {
   return (
-    <header className="px-4 flex justify-between items-center bg-accentThree text-[191919] py-3 shadow-md">
-      <div className="flex items-center">
-        <div className=" border border-spacing-12 border-primary rounded-full w-[15%] p-2">
-          <Image src={PawVet} alt="Pata de pet" sizes="100%" />
+    <>
+      <header className="px-4 flex justify-between items-center bg-accentThree text-[191919] py-3 shadow-md">
+        <div className="flex items-center">
+          <div className=" border border-spacing-12 border-primary rounded-full w-[15%] p-2">
+            <Image src={PawVet} alt="Pata de pet" sizes="100%" />
+          </div>
+          <div className="w-[50%]">
+            <Image src={Logo} alt="Logo Mascots" sizes="100%" />
+          </div>
         </div>
-        <div className="w-[50%]">
-          <Image src={Logo} alt="Logo Mascots" sizes="100%" />
+        <div className="flex items-center gap-4">
+          <div className="w-[15%] p-1 hover:cursor-pointer">
+            <Image src={Notification} alt="Ícone de notificação" sizes="100%" />
+          </div>
+          <Input placeholder={`Pesquise aqui...`} label={""} />
         </div>
+      </header>
+      <div className="flex my-4 w-[50%]">
+        <Button label={"+ Consulta"} />
       </div>
-      <div className="flex items-center gap-4">
-        <div className="w-[15%] p-1 hover:cursor-pointer">
-          <Image src={Notification} alt="Ícone de notificação" sizes="100%" />
-        </div>
-        <Input placeholder={`Pesquise aqui...`} label={""} />
-      </div>
-    </header>
+    </>
   );
 };
 
