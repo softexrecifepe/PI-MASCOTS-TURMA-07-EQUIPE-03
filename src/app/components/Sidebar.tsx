a4836061ea5b923598da27ec2663d3a73867ed62
hudney-fsbrito <hudney.rse@gmail.com>
2024-11-10 16:05:26 -0300

    feat: cria dropdown no sidbar e add ícones

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,9 +1,13 @@
-'use client';
-import React from "react";
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
 
 interface LinkItem {
+  icon?: string;
   name: string;
   href: string;
+  sublinks?: LinkItem[];
 }
 
 interface SidebarProps {
@@ -12,24 +16,66 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ links, onClickLink }) => {
+  const [openDropdowns, setOpenDropdowns] = useState<{
+    [key: string]: boolean;
+  }>({});
+
+  const toggleDropdown = (name: string) => {
+    setOpenDropdowns((prevState) => ({
+      ...prevState,
+      [name]: !prevState[name],
+    }));
+  };
+
   return (
     <nav>
       <ul>
         {links.map((link, index) => (
-          <li key={index} className="mb-1">
-            <a
-              onClick={(e) => {
-                e.preventDefault(); // Impede o redirecionamento imediato
-                if (onClickLink) {
-                  onClickLink(link); // Chama a função passada por prop, se existir
-                }
-              }}
-              href={link.href}
-              className="p-2 rounded block transition duration-200 hover:bg-gray-300 hover:shadow-md hover:text-gray-900"
-            >
-              {link.name}
-            </a>
-          </li>
+          <>
+            <li key={index} className="flex items-center justify-start ps-2 rounded-md transition duration-200 hover:bg-gray-300 hover:shadow-md hover:text-gray-900">
+            {link.icon && (
+                  <Image
+                    src={link.icon}
+                    alt={`${link.name} Icon`}
+                    width={20} // ajuste o tamanho conforme necessário
+                    height={20}
+                  />
+                )}
+              <Link
+                onClick={(e) => {
+                  e.preventDefault(); // Impede o redirecionamento imediato
+                  if (onClickLink) {
+                    onClickLink(link); // Chama a função passada por prop, se existir
+                  }
+                }}
+                href={link.href}
+                className="p-2 rounded block "
+              >
+                {link.name}
+              </Link>
+              {link.sublinks && (
+                <button
+                  onClick={() => toggleDropdown(link.name)}
+                  className="text-gray-400 hover:shadow-md hover:bg-gray-300 hover:text-gray-900 "
+                >
+                  {openDropdowns[link.name] ? "▲" : "▼"}
+                </button>
+              )}
+            </li>
+            {link.sublinks && openDropdowns[link.name] && (
+              <div className="ml-4 flex flex-col space-y-1 ">
+                {link.sublinks.map((sublink) => (
+                  <Link
+                    key={sublink.name}
+                    href={sublink.href}
+                    className="text-gray-600  hover:bg-gray-300 hover:shadow-md hover:text-gray-900 transition duration-300"
+                  >
+                    {sublink.name}
+                  </Link>
+                ))}
+              </div>
+            )}
+          </>
         ))}
       </ul>
     </nav>
--- src/app/management/layout.tsx
+++ src/app/management/layout.tsx
@@ -3,29 +3,44 @@ import HeaderManagement from "../components/HeaderManagement";
 import Image from "next/image";
 import { Sidebar } from "../components/Sidebar";
 import { Button } from "../components/Button";
-import plusIcon from "../../../public/plus-square1.svg";  
+import plusIcon from "../../../public/plus-square1.svg";
 
 interface ManagementLayoutProps {
   children: ReactNode;
 }
 
 const ManagementLayout: React.FC<ManagementLayoutProps> = ({ children }) => {
-
-    const linksSidebar = [
-        { name: "Home", href: "/management" },
-        { name: "Cadastros", href: "/management/" },
-        { name: "Internamento", href:"#"},
-        { name: "Exames", href: "#" },
-        { name: "Agenda", href: "#" },
-        { name: "Inventário", href: "#" },
-      ];
-    
+  const linksSidebar = [
+    { icon: "/home.svg", name: "Home", href: "/management" },
+    {
+      icon: "/file-tray-full.svg",
+      name: "Cadastros",
+      href: "/management/",
+      sublinks: [
+        { name: "Novo tutor", href: "/management/#" },
+        { name: "Novo animal", href: "/management/#" },
+      ],
+    },
+    {
+      icon: "/medical.svg",
+      name: "Internamento",
+      href: "#",
+      sublinks: [
+        { name: "Internar novo animal", href: "/management/#" },
+        { name: "Novo tratamento", href: "/management/#" },
+        { name: "Iniciar novo monitoramento", href: "/management/#" },
+      ],
+    },
+    { icon: "/file.svg", name: "Exames", href: "#" },
+    { icon: "/calendar.svg", name: "Agenda", href: "#" },
+    { icon: "/box.svg", name: "Inventário", href: "#" },
+  ];
 
   return (
     <div className="h-screen flex flex-col">
       <HeaderManagement />
       <div className="flex h-screen overflow-hidden">
-      <div className="flex flex-col w-[20%] ms-2 px-2">
+        <div className="flex flex-col w-[20%] ms-2 px-2">
           <div className="flex my-2 w-full  ">
             <Button label={"Agendar consulta"}>
               <div className="w-11 px-1">
@@ -34,9 +49,8 @@ const ManagementLayout: React.FC<ManagementLayoutProps> = ({ children }) => {
             </Button>
           </div>
           <Sidebar links={linksSidebar} />
-          </div>
-        <main className="flex-1 h-full overflow-y-auto">{children}
-        </main>
+        </div>
+        <main className="flex-1 h-full overflow-y-auto">{children}</main>
       </div>
     </div>
   );
